3973c0d6f0a3e4e75b609d5b56ad8cb21b732b18
Doug Duril <doug.duril@gmail.com>
2016-05-23 20:16:29 -0700

    Added node-express-gen example

---
+++ 5-server-side-nodejs/node-express-gen/routes/leaderRouter.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var leaderRouter = express.Router();
+
+
+leaderRouter.use(bodyParser.json());
+
+
+leaderRouter.route('/')
+.get(function(req,res,next){
+    res.end('Will send all the leaders to you!');
+})
+
+.post(function(req, res, next){
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' +         req.body.description);    
+})
+
+.delete(function(req, res, next){
+    res.end('Deleting all leaders');
+});
+
+
+leaderRouter.route('/:leaderId')
+.get(function(req,res,next){
+    res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+})
+
+.put(function(req, res, next){
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+    res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+
+module.exports = leaderRouter;
---
+++ 5-server-side-nodejs/node-express-gen/routes/promoRouter.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var promoRouter = express.Router();
+
+
+promoRouter.use(bodyParser.json());
+
+
+promoRouter.route('/')
+.get(function(req,res,next){
+    res.end('Will send all the promotions to you!');
+})
+
+.post(function(req, res, next){
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' +         req.body.description);    
+})
+
+.delete(function(req, res, next){
+    res.end('Deleting all promotions');
+});
+
+
+promoRouter.route('/:promoId')
+.get(function(req,res,next){
+    res.end('Will send details of the promotion: ' + req.params.promoId +' to you!');
+})
+
+.put(function(req, res, next){
+    res.write('Updating the promotion: ' + req.params.promoId + '\n');
+    res.end('Will update the promotion: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+    res.end('Deleting promotion: ' + req.params.promoId);
+});
+
+
+module.exports = promoRouter;
